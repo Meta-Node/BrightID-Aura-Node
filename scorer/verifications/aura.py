521068dbd8c7234f2bd7b4b750319fcc19bfd0f7
Sina <snparvizi75@gmail.com>
2022-11-17 17:20:31 +0330

    calculate inflow and outflow energy and store them in energyTotals collection

--- scorer/verifications/aura.py
+++ scorer/verifications/aura.py
@@ -4,18 +4,18 @@ import os
 import time
 
 client = ArangoClient(hosts=config.ARANGO_SERVER)
-system = client.db('_system')
-snapshot = client.db('snapshot')
+system = client.db("_system")
+snapshot = client.db("snapshot")
 
 # Constants related to arangodump
 
-AURA_SNAPSHOT_DIR = f'/tmp/aura'
+AURA_SNAPSHOT_DIR = f"/tmp/aura"
 
 # Constants related to Aura scores
 
 ENERGY_TEAM = [
-    'xqmMHQMnBdakxs3sXXjy7qVqPoXmhhwOt4c_z1tSPwM',
-    'AsjAK5gJ68SMYvGfCAuROsMrJQ0_83ZS92xy94LlfIA',
+    "xqmMHQMnBdakxs3sXXjy7qVqPoXmhhwOt4c_z1tSPwM",
+    "AsjAK5gJ68SMYvGfCAuROsMrJQ0_83ZS92xy94LlfIA",
 ]
 STARTING_ENERGY = 10000000
 HOPS = 4
@@ -31,45 +31,50 @@ BRONZE = 1000
 
 
 def verify(block):
-    print('Verification: Aura')
+    print("Verification: Aura")
 
     # Create collections if needed
 
-    if not snapshot.has_collection('energy'):
-        snapshot.create_collection('energy')
+    if not snapshot.has_collection("energy"):
+        snapshot.create_collection("energy")
 
-    if not snapshot.has_collection('energyNext'):
-        snapshot.create_collection('energyNext')
+    if not snapshot.has_collection("energyNext"):
+        snapshot.create_collection("energyNext")
 
-    if not snapshot.has_collection('energyFlow'):
-        energy_flow = snapshot.create_collection('energyFlow', edge=True)
-        energy_flow.add_persistent_index(fields=['timestamp'])
+    if not snapshot.has_collection("energyFlow"):
+        energy_flow = snapshot.create_collection("energyFlow", edge=True)
+        energy_flow.add_persistent_index(fields=["timestamp"])
 
-    if not snapshot.has_collection('aura'):
-        snapshot.create_collection('aura')
+    if not snapshot.has_collection("aura"):
+        snapshot.create_collection("aura")
 
-    energy = snapshot.collection('energy')
-    energy_next = snapshot.collection('energyNext')
-    aura = snapshot.collection('aura')
+    if not snapshot.has_collection("energyTotals"):
+        snapshot.create_collection("energyTotals")
+
+    energy = snapshot.collection("energy")
+    energy_next = snapshot.collection("energyNext")
+    aura = snapshot.collection("aura")
+    enegry_totals = snapshot.collection("energyTotals")
 
     # Clear collections
 
     energy.truncate()
     energy_next.truncate()
     aura.truncate()
+    enegry_totals.truncate()
 
     # Initialize the energy
 
-    snapshot.aql.execute('''
+    snapshot.aql.execute(
+        """
         for e in @energyTeam
             insert {
                 _key: e,
                 energy: @startingEnergy
             } in energy
-    ''', bind_vars={
-        "energyTeam": ENERGY_TEAM,
-        "startingEnergy": STARTING_ENERGY
-    })
+    """,
+        bind_vars={"energyTeam": ENERGY_TEAM, "startingEnergy": STARTING_ENERGY},
+    )
 
     # Flow the energy
 
@@ -78,7 +83,8 @@ def verify(block):
     for i in range(HOPS):
 
         if i < HOPS - 1:
-            snapshot.aql.execute('''
+            snapshot.aql.execute(
+                """
                 for e in energy
                     let scale = sum (
                         for ea in energyAllocation
@@ -92,9 +98,11 @@ def verify(block):
                         insert { _key: key, energy: e.energy * ea.allocation / scale }
                         update { energy: OLD.energy + e.energy * ea.allocation / scale }
                         in energyNext
-            ''')
+            """
+            )
         else:  # Store transfers on the last hop for data visualization
-            snapshot.aql.execute('''
+            snapshot.aql.execute(
+                """
                 for e in energy
                     let scale = sum (
                         for ea in energyAllocation
@@ -111,16 +119,17 @@ def verify(block):
                         in energyNext
                         insert { _from: ea._from, _to: ea._to, energy: energySent, timestamp: @timestamp }
                         in energyFlow
-            ''', bind_vars={
-                "timestamp": timestamp
-            })
+            """,
+                bind_vars={"timestamp": timestamp},
+            )
 
         # Clean up old energyFlow data
         #
         # Remove the rows with the middle timestamp from today (if it exists)
         # leaving only the most recent and least recent rows.
 
-        snapshot.aql.execute('''
+        snapshot.aql.execute(
+            """
              let timesToday = (
                 for ef in energyFlow
                     filter ef.timestamp < @timestamp
@@ -132,20 +141,44 @@ def verify(block):
             for ef in energyFlow
                 filter ef.timestamp == timesToday[1].timestamp
                 remove ef in energyFlow
-        ''', bind_vars={
-            "timestamp": timestamp
-        })
+        """,
+            bind_vars={"timestamp": timestamp},
+        )
 
         energy.truncate()
-        energy.rename('energyTemp')
-        energy_next.rename('energy')
-        energy.rename('energyNext')
-        energy = snapshot.collection('energy')
-        energy_next = snapshot.collection('energyNext')
+        energy.rename("energyTemp")
+        energy_next.rename("energy")
+        energy.rename("energyNext")
+        energy = snapshot.collection("energy")
+        energy_next = snapshot.collection("energyNext")
+
+    # Energy flow table is clean now
+    # use this data to comput total energy flow to each aura player
+    # we need to sum all row with the same _to and _timestamp to get total inflow energy
+
+    snapshot.aql.execute(
+        """
+        for ef in energyFlow
+            let key = split(ef._to,'/',2)[1]
+            let inflow = sum (
+                for eft in energyFlow
+                    filter eft._to == key and eft.timestamp == ef.timestamp
+                    return eft.energy
+            )
+            let outflow = sum (
+                for eft in energyFlow
+                    filter eft._from == key and eft.timestamp == ef.timestamp
+                    return eft.energy
+            )
+            insert { _key: key, inflowEnergy: inflow, outflowEnergy: outflow, timestamp: ef.timestamp }
+            in energyTotals
+    """
+    )
 
     # Compute Aura scores
 
-    snapshot.aql.execute('''
+    snapshot.aql.execute(
+        """
         let ratingCutoff = date_subtract(date_now(), @ratingCutoffHours, "hours")
     
         for h in honesty
@@ -171,18 +204,20 @@ def verify(block):
                     score: score
                 } in aura
                 
-    ''', bind_vars={
-        "flaggingMultiplier": FLAGGING_MULTIPLIER,
-        "ratingCutoffHours": RATING_CUTOFF_HOURS,
-        "allowedRatings": ALLOWED_RATINGS_PER_CUTOFF,
-    })
+    """,
+        bind_vars={
+            "flaggingMultiplier": FLAGGING_MULTIPLIER,
+            "ratingCutoffHours": RATING_CUTOFF_HOURS,
+            "allowedRatings": ALLOWED_RATINGS_PER_CUTOFF,
+        },
+    )
 
     # Transfer the aura collection from snapshot to _system
 
     result = os.system(
         f'arangodump --overwrite true --compress-output false --server.password ""'
         f' --server.endpoint "tcp://{config.BN_ARANGO_HOST}:{config.BN_ARANGO_PORT}"'
-        f' --output-directory {AURA_SNAPSHOT_DIR} --server.database snapshot --collection aura'
+        f" --output-directory {AURA_SNAPSHOT_DIR} --server.database snapshot --collection aura"
     )
     assert result == 0, "Aura: dumping aura collection failed."
     result = os.system(
@@ -193,7 +228,8 @@ def verify(block):
 
     # Write the verifications
 
-    system.aql.execute('''
+    system.aql.execute(
+        """
         for a in aura
         let level =
             a.score > @gold ? "Gold" :
@@ -208,12 +244,14 @@ def verify(block):
             score: a.score,
             level: level
         } into verifications
-    ''', bind_vars={
-        "gold": GOLD,
-        "silver": SILVER,
-        "bronze": BRONZE,
-        "block": block,
-    })
+    """,
+        bind_vars={
+            "gold": GOLD,
+            "silver": SILVER,
+            "bronze": BRONZE,
+            "block": block,
+        },
+    )
 
 
 if __name__ == "__main__":
